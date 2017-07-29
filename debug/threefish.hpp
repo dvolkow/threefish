10da9e822f04fda9ef96e0256a2563f5439068e1
Daniel Wolkow <volkov12@rambler.ru>
2017-07-29 17:54:21 +0300

    change interface

--- debug/threefish.hpp
+++ debug/threefish.hpp
@@ -2,9 +2,9 @@
 #define THREEFISH_HPP 
 
 #include <cstdint>
+#include <cstring>
 #include <utility>
 #include <vector>
-#include <iostream>
 
 namespace threefish 
 {
@@ -20,6 +20,7 @@ namespace threefish
     // permutation constants
     template<uint8_t> struct inverstions_table;
 
+    // byte rotation left-right
     inline uint64_t rol(const uint64_t n, const uint8_t shift)
     {
         return (n << shift) | (n >> (64 - shift));
@@ -30,6 +31,7 @@ namespace threefish
         return (n << (64 - shift)) | (n >> shift);
     }
 
+    // MIX function
     inline void mix(uint64_t x0, uint64_t x1, uint64_t & y0, uint64_t & y1, uint8_t r)
     {
         y0 = x0 + x1;
@@ -54,134 +56,47 @@ namespace threefish
             demix(v[p[i]], v[p[i + 1]], v[p[i]], v[p[i + 1]], r[i / 2]);
     }
 
-    /*
-    void threefish_exp(const uint8_t nw, const uint8_t nr, uint64_t * key, uint64_t * tweak, uint64_t ** subkeys)
-    {
-        uint64_t xkey[nw + 1];
-        uint64_t xtweak[3] = {tweak[0], tweak[1], tweak[0] ^ tweak[1]};
-
-        xkey[nw] = C240;
-        for (uint8_t i = 0; i < nw; ++i)
-            xkey[i] = key[i], xkey[nw] ^= key[i];
-
-        for (uint8_t i = 0; i < nr / 4 + 1; ++i)
-        {
-            for (uint8_t j = 0; j < nw; ++j)
-                subkeys[i][j] = xkey[(i + j) % (nw + 1)];
-
-            subkeys[i][nw - 3] += xtweak[i % 3];
-            subkeys[i][nw - 2] += xtweak[(i + 1) % 3];
-            subkeys[i][nw - 1] += i;
-        }
-    }
-
-    void encrypt_generic(const uint8_t nw, const uint8_t nr, const uint8_t ** rot, 
-                         const uint8_t ** p, uint8_t * key, uint64_t * tweak,
-                         uint64_t * plaintext, uint64_t * ciphertext)
-    {
-        uint64_t subkeys[nr/4 + 1][nw];
-//        threefish_exp(nw, nr, key, tweak, &subkeys);
-
-        uint64_t v[nw];
-
-	    for (uint8_t i = 0; i < nw; ++i) 
-		    v[i] = plaintext[i];
-        
-        for (uint8_t n = 0; n < nr; n += 8) 
-        {
-		    for (uint8_t w = 0; w < nw; ++w) 
-			    v[w] += subkeys[n / 4][w];
-		
-            encrypted_round_mix(nw, v, rot[(n + 0) % 8], p[0]);
-            encrypted_round_mix(nw, v, rot[(n + 1) % 8], p[1]);
-            encrypted_round_mix(nw, v, rot[(n + 2) % 8], p[2]);
-            encrypted_round_mix(nw, v, rot[(n + 3) % 8], p[3]);
-
-            for (uint8_t w = 0; w < nw; ++w) 
-                v[w] += subkeys[n / 4 + 1][w];
-		    
-            encrypted_round_mix(nw, v, rot[(n + 4) % 8], p[0]);
-            encrypted_round_mix(nw, v, rot[(n + 5) % 8], p[1]);
-            encrypted_round_mix(nw, v, rot[(n + 6) % 8], p[2]);
-            encrypted_round_mix(nw, v, rot[(n + 7) % 8], p[3]);
-        }
-
-        for (unsigned w = 0; w < nw; ++w) 
-            ciphertext[w] = v[w] + subkeys[nr/4][w];
-    }
-
-    void decrypt_generic(const uint8_t nw, const uint8_t nr, const uint8_t ** rot, 
-                         const uint8_t ** p, uint8_t * key, uint64_t * tweak,
-                         uint64_t * ciphertext, uint64_t * plaintext)
-    {
-        uint64_t subkeys[nr/4 + 1][nw];
- //       threefish_exp(nw, nr, key, tweak, &subkeys);
-
-        uint64_t v[nw];
-
-        for (uint8_t i = 0; i < nw; ++i) 
-            v[i] = ciphertext[i] - subkeys[nr / 4][i];
-        
-        for (uint8_t n = nr - 8; n != 0; n -= 8) 
-        {
-            decrypted_round_mix(nw, v, rot[(n + 7) % 8], p[3]);
-            decrypted_round_mix(nw, v, rot[(n + 6) % 8], p[2]);
-            decrypted_round_mix(nw, v, rot[(n + 5) % 8], p[1]);
-            decrypted_round_mix(nw, v, rot[(n + 4) % 8], p[0]);
-
-            for (uint8_t w = 0; w < nw; ++w) 
-                v[w] -= subkeys[n / 4 + 1][w];
-
-            decrypted_round_mix(nw, v, rot[(n + 3) % 8], p[3]);
-            decrypted_round_mix(nw, v, rot[(n + 2) % 8], p[2]);
-            decrypted_round_mix(nw, v, rot[(n + 1) % 8], p[1]);
-            decrypted_round_mix(nw, v, rot[(n + 0) % 8], p[0]);
-
-            for (uint8_t w = 0; w < nw; ++w) 
-                v[w] -= subkeys[n / 4][w];
-        }
-
-        for (uint8_t w = 0; w < nw; ++w) 
-            plaintext[w] = v[w];
-    }
-
-    */
-
     template <uint8_t SIZE_BLOCK>
-    class base 
+    class Cryptor 
     {
         typedef rotation_table<SIZE_BLOCK> r_type;
         typedef inverstions_table<SIZE_BLOCK> inv_type;
 
     public:
-        base(uint64_t * key) 
+        Cryptor(uint64_t * key) 
             : plaintext_(SIZE_BLOCK), ciphertext_(SIZE_BLOCK), subkeys_(), tweak_(), nw_(SIZE_BLOCK), nr_(SIZE_BLOCK < 16 ? 72 : 80) 
             {
                 keys_expand(key);
             } 
 
-        void encrypt(const size_t count_blocks, uint64_t * text)
+        void encrypt(const size_t count_blocks, uint64_t * source, uint64_t * dest)
         {
             for (size_t i = 0; i < count_blocks; ++i)
-                encrypt_block(text + i * SIZE_BLOCK);
+            {
+                encrypt_block(source + i * SIZE_BLOCK);
+                std::memcpy(dest + i * SIZE_BLOCK, &ciphertext_[0], ciphertext_.size() * sizeof(ciphertext_[0]));
+            }
         }
 
-        void decrypt(const size_t count_blocks, uint64_t * text)
+        void decrypt(const size_t count_blocks, uint64_t * source, uint64_t * dest)
         {
             for (size_t i = 0; i < count_blocks; ++i)
-                decrypt_block(text + i * SIZE_BLOCK);
+            {
+                decrypt_block(source + i * SIZE_BLOCK);
+                std::memcpy(dest + i * SIZE_BLOCK, &plaintext_[0], plaintext_.size() * sizeof(plaintext_[0]));
+            }
         }
 
     private:
         void keys_expand(uint64_t * key);
-    public:
         void encrypt_block(uint64_t * block);
         void decrypt_block(uint64_t * block);
 
+    private:
         vector<uint64_t> plaintext_;
         vector<uint64_t> ciphertext_;
-    private:
         vector<vector<uint64_t>> subkeys_;
+
         uint64_t tweak_[3]; 
         uint8_t nw_, nr_;
         r_type rot_;
@@ -189,7 +104,7 @@ namespace threefish
     };
 
     template<uint8_t SIZE_BLOCK>
-    void base<SIZE_BLOCK>::keys_expand(uint64_t * key)
+    void Cryptor<SIZE_BLOCK>::keys_expand(uint64_t * key)
     {
         subkeys_ = vector<vector<uint64_t>>(nr_ / 4 + 1, vector<uint64_t>(nw_));
         
@@ -211,7 +126,7 @@ namespace threefish
     }
 
     template<uint8_t SIZE_BLOCK>
-    void base<SIZE_BLOCK>::encrypt_block(uint64_t * block)
+    void Cryptor<SIZE_BLOCK>::encrypt_block(uint64_t * block)
     {
         vector<uint64_t> v(nw_);
 
@@ -242,7 +157,7 @@ namespace threefish
     }
 
     template<uint8_t SIZE_BLOCK>
-    void base<SIZE_BLOCK>::decrypt_block(uint64_t * block)
+    void Cryptor<SIZE_BLOCK>::decrypt_block(uint64_t * block)
     {
         vector<uint64_t> v(nw_);
         for (uint8_t i = 0; i < nw_; ++i) 
--- test/smoke_test.cpp
+++ test/smoke_test.cpp
@@ -7,10 +7,13 @@ int main()
 {
 
     uint64_t key[16] = {0};
-	uint64_t plaintext[16] = {0};
-    threefish::base<16> enc(&key[0]);
-    enc.encrypt_block(&plaintext[0]);
-    enc.decrypt_block(&enc.ciphertext_[0]);
+	uint64_t plaintext[32] = {0};
+	uint64_t cypertext[32] = {0};
+	uint64_t decrypt[32] = {0};
+
+    threefish::Cryptor<16> enc(&key[0]);
+    enc.encrypt(2, &plaintext[0], &cypertext[0]);
+    enc.decrypt(2, &cypertext[0], &decrypt[0]);
 
 
     std::ofstream plaintext_file;
@@ -20,15 +23,15 @@ int main()
     std::ofstream decrypt_file;
     decrypt_file.open("decrypt.txt", std::ios::binary);
 
-    for (uint8_t i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 32; ++i)
     {
         plaintext_file << plaintext[i];
     }
 
-    for (uint8_t i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 32; ++i)
     {
-        cypertext_file << enc.ciphertext_[i];
-        decrypt_file << enc.plaintext_[i];
+        cypertext_file << cypertext[i];
+        decrypt_file << decrypt[i];
     }
 //    std::cout << static_cast<char>()
  //   std::cout << static_cast<int>(threefish::rotation_table<16>::arr[2][1]) << " " << a << "\n";
